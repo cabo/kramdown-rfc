5adfdb225e4c0a7ff6362f7cbb1da4ddb8ef6b1f
Carsten Bormann <cabo@tzi.org>
2011-01-03 18:39:50 +0100

    Add boneheaded include functionality

--- convert1.rb
+++ convert1.rb
@@ -8,7 +8,10 @@ require 'yaml'
 Encoding.default_external = "UTF-8" # wake up, smell the coffee
 
 options = {input: 'RFC2629Kramdown'}
-input = "{::nomarkdown}\n#{ARGF.read}\n{:/nomarkdown}\n"
+input = ARGF.read.gsub(/\{::include\s+(.*?)\}/) {
+  File.read($1).chomp
+}
+input = "{::nomarkdown}\n#{input}\n{:/nomarkdown}\n"
 doc = Kramdown::Document.new(input, options)
 $stderr.puts doc.warnings.to_yaml unless doc.warnings.empty?
 puts doc.to_rfc2629
