2d60dadbe920bb89d0d67d71ea03d1fea5590a48
David Schinazi <DavidSchinazi@users.noreply.github.com>
2024-11-11 11:00:41 +0000

    Allow configuring REFCACHETTL for all document types
    Before this commit, most but not all document ref cache TTLs could be configured. This change extends that functionality to other document types via two additional environment variables.

--- lib/kramdown-rfc2629.rb
+++ lib/kramdown-rfc2629.rb
@@ -1306,9 +1306,12 @@ COLORS
          "#{XML_RESOURCE_ORG_PREFIX}/bibxml-rfcsubseries/#{name}"] # FOR NOW
       end
 
+      KRAMDOWN_REFCACHETTL_RFC = (e = ENV["KRAMDOWN_REFCACHETTL_RFC"]) ? e.to_i : 86400*7
+      KRAMDOWN_REFCACHETTL_DOI_IANA = (e = ENV["KRAMDOWN_REFCACHETTL_DOI_IANA"]) ? e.to_i : 86400
+
       # [subdirectory name, cache ttl in seconds, does it provide for ?anchor=]
       XML_RESOURCE_ORG_MAP = {
-        "RFC" => ["bibxml", 86400*7, false,
+        "RFC" => ["bibxml", KRAMDOWN_REFCACHETTL_RFC, false,
                   ->(fn, n){ [name = "reference.RFC.#{"%04d" % n.to_i}.xml",
                               "https://bib.ietf.org/public/rfc/bibxml/#{name}"] }
 # was                         "https://www.rfc-editor.org/refs/bibxml/#{name}"] }
@@ -1317,10 +1320,10 @@ COLORS
                   ->(fn, n){ [fn,
                               "https://datatracker.ietf.org/doc/bibxml3/draft-#{n.sub(/\Adraft-/, '')}.xml"] }
                  ],
-        "BCP" => ["bibxml-rfcsubseries", 86400*7, false,
+        "BCP" => ["bibxml-rfcsubseries", KRAMDOWN_REFCACHETTL_RFC, false,
                   ->(fn, n){ Rfc2629::bcp_std_ref("BCP", n) }
                  ],
-        "STD" => ["bibxml-rfcsubseries", 86400*7, false,
+        "STD" => ["bibxml-rfcsubseries", KRAMDOWN_REFCACHETTL_RFC, false,
                   ->(fn, n){ Rfc2629::bcp_std_ref("STD", n) }
                  ],
         "W3C" => "bibxml4",
@@ -1336,9 +1339,10 @@ COLORS
         "NIST" => "bibxml2",
         "OASIS" => "bibxml2",
         "PKCS" => "bibxml2",
-        "DOI" => ["bibxml7", 86400, true, ->(fn, n){ ["computed-#{fn}", [:DOI, n] ] }, true # always_altproc
+        "DOI" => ["bibxml7", KRAMDOWN_REFCACHETTL_DOI_IANA, true,
+                  ->(fn, n){ ["computed-#{fn}", [:DOI, n] ] }, true # always_altproc
                  ], # emulate old 24 h cache
-        "IANA" => ["bibxml8", 86400, true], # ditto
+        "IANA" => ["bibxml8", KRAMDOWN_REFCACHETTL_DOI_IANA, true], # ditto
       }
 
       # XML_RESOURCE_ORG_HOST = ENV["XML_RESOURCE_ORG_HOST"] || "xml.resource.org"
